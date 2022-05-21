0f8e5de80a8e42c777fd488f6496369f5d429540
OussamaBouazza <oussama-bouazza@outlook.com>
2022-05-21 15:49:53 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'f1-tracker',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
